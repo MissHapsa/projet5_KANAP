793e9fe69050e08ef1209f7a23853f029b1821de
Misshapsa <kontehapsatou@gmail.com>
2022-10-31 18:28:01 +0100

    first

---
+++ frontend/js/script.js
@@ -0,0 +1,39 @@
+//------------------------------------------------------------------------
+// Récupération des produits via l'api //
+
+fetch("http://localhost:3000/api/products")
+  // réponse donne le résultat en json.
+  .then((res) => res.json())
+  //  resulat reçu et traité en json = objectProducts
+  .then((objectProducts) => {
+    //  en console : les donnees récupérées dans un tableau.
+    console.table(objectProducts);
+    // appel de la fonction d'affichage des produits
+    lesKanaps(objectProducts);
+  })
+  // si erreur remplace le titre par un h1 "erreur 404" et affiche la dans la console.
+  .catch((err) => {
+    document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
+    console.log("erreur 404, sur ressource api:" + err);
+  });
+//----------------------------------------------------------------------
+
+// fonction d'affichage des produits de l'api dans l index.html
+//----------------------------------------------------------------------
+function lesKanaps(index) {
+    // variable de la zone d'article
+    let zoneArticle = document.querySelector("#items");
+    // boucle pour chaque indice('article') dans index.html
+    for (let article of index) {
+      /* création et ajout des zones d'articles, insertion de l'adresse produit via chemin produit + paramètres(son id);
+      la page index est http://127.0.0.1:5500/front/html/index.html donc la page du produit sera http://127.0.0.1:5500/front/html/product.html 
+      (d'ou le ./product.html) pour rajouter son paramètre on met ? puis la clé (ici _id) associé (=) à sa valeur dynamique ${article._id} */
+      zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+      <article>
+        <img src="${article.imageUrl}" alt="${article.altTxt}">
+        <h3 class="productName">${article.name}</h3>
+        <p class="productDescription">${article.description}</p>
+      </article>
+    </a>`;
+    }
+  }
