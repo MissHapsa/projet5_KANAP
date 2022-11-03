d56abae601992aa9960108103a738cad2c9ede42
Misshapsa <kontehapsatou@gmail.com>
2022-11-03 19:49:52 +0100

    creation du panier etape 1 ok

---
+++ frontend/js/cart.js
@@ -0,0 +1,21 @@
+// pour différancier la page confirmation et panier
+const page = document.location.href;
+//-----------------------------------
+// Récupération des produits de l'api
+//-----------------------------------
+// appel de la ressource api product (script.j) si on est sur la page panier
+if (page.match("cart")) {
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
+  .then((objetProduits) => {
+      console.log(objetProduits);
+      // appel de la fonction affichagePanier
+      affichagePanier(objetProduits);
+  })
+  .catch((err) => {
+      document.querySelector("#cartAndFormContainer").innerHTML = "<h1>erreur 404</h1>";
+      console.log("erreur 404, sur ressource api: " + err);
+  });
+} else {
+  console.log("sur page confirmation");
+}
--- frontend/js/product.js
+++ frontend/js/product.js
@@ -1,36 +1,36 @@
-//--------------------------------------------------------------------------
+//------------------------------------------
 // Récupération de l'id du produit via l' URL
-//--------------------------------------------------------------------------
-//la variable params récupère l'url de la page   https://qastack.fr/programming/9870512/how-to-obtain-the-query-string-from-the-current-url-with-javascript
-const params = new URLSearchParams(document.location.search); //console.log(document.location);  https://developer.mozilla.org/fr/docs/Web/API/Document/location
-// la variable id va récupérer la valeur du paramètre _id
+//-------------------------------------------
+const params = new URLSearchParams(document.location.search);
+// la variable id va récupérer _id
 const id = params.get("_id");
 console.log(id); 
+
 //--------------------------------------------------------------------------
-// Récupération des produits de l'api et traitement des données (voir script.js)
+// Récupération des produits de l'api et traitement des données (script.js)
 //--------------------------------------------------------------------------
 fetch("http://localhost:3000/api/products")
   .then((res) => res.json())
   .then((objectProducts) => {
     // execution de la fontion lesProduits
-    lesProduits(objectProducts);
+    lesKanaps(objectProducts);
   })
   .catch((err) => {
     document.querySelector(".item").innerHTML = "<h1>erreur 404</h1>";
     console.log("erreur 404, sur ressource api: " + err);
   });
-//------------------------------------------------------------------------
+//-------------------------------
 // Création d'objet articleClient
-//------------------------------------------------------------------------
-// déclaration objet articleClient prêt à être modifiée par les fonctions suivantes d'évènements
+//-------------------------------
+// déclaration objet articleClient prêt à être modifiée
 let articleClient = {};
 // id du procuit
 articleClient._id = id;
-//------------------------------------------------------------------------
+//------------------------------------------
 // fonction d'affichage du produit de l'api
-//------------------------------------------------------------------------
-function lesProduits(produit) {
-  // déclaration des variables pointage des éléments
+//------------------------------------------
+function lesKanaps(produit) {
+  // variables des éléments
   let imageAlt = document.querySelector("article div.item__img");
   let titre = document.querySelector("#title");
   let prix = document.querySelector("#price");
@@ -38,7 +38,7 @@ function lesProduits(produit) {
   let couleurOption = document.querySelector("#colors");
   // boucle for pour chercher un indice
   for (let choix of produit) {
-    //si id (définit par l'url) est identique à un _id d'un des produits du tableau, on récupère son indice de tableau qui sert pour les éléments produit à ajouter
+    //si id est identique à un _id d'un des produits du tableau, on récupère son indice de tableau qui sert pour les éléments produit à ajouter
     if (id === choix._id) {
       //ajout des éléments de manière dynamique
       imageAlt.innerHTML = `<img src="${choix.imageUrl}" alt="${choix.altTxt}">`;
